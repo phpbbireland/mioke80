1e656ba3e9123b91a3e01724078341a8a47f5b86
Michael O'Toole <phpbbireland@gmail.com>
2024-08-23 00:43:14 +0100

    Create config.h

---
+++ blackpill_version/QMK/ayla_411/config.h
@@ -0,0 +1,20 @@
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/* key matrix size */
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 10
+
+#define RGB_TRIGGER_ON_KEYDOWN
+
+#define NO_DEBUG
+#define NO_PRINT
