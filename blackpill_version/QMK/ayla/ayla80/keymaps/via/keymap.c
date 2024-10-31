fbaf5947e1d02f2001feb96cb32a7c94cc2ece5f
Michael O'Toole <phpbbireland@gmail.com>
2024-10-31 13:24:24 +0000

    Update keymap.c

--- blackpill_version/QMK/ayla/ayla80/keymaps/via/keymap.c
+++ blackpill_version/QMK/ayla/ayla80/keymaps/via/keymap.c
@@ -1,12 +1,15 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 // see keyboard.info
+#include "kb.h"
 
 #include QMK_KEYBOARD_H
 
 enum layer_names {
     _BASE,
     _FN,
+    _ME,
+    _SP
 };
 
 
@@ -24,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // use this for new discrete pcb, else fix 1 above to compile for blackpill first board
 
     /*
-    [_BASE] = LAYOUT_411_ISO(
+    [_BASE] = LAYOUT_411_iso(
                 KC_TRNS, KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SCRL,   KC_PAUSE,
                 KC_TRNS, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_INSERT, KC_HOME,
                 KC_PGUP, KC_TRNS, KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_DEL,    KC_END,
@@ -49,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        see https://github.com/phpbbireland/mioke80/blob/main/blackpill_version/images/ayla80v2.png
        For ANSI layout, this may/will change...
 
-       LAYOUT name i.e. "LAYOUT_411_iso" should match in keyboard.json file
+       * LAYOUT name i.e. "LAYOUT_411_iso" should match in keyboard.json file * KC_ENT KC_BSLS
     */
 
     /*  Row:    0        1        2          3        4         5         6         7        8        9 */
@@ -60,9 +63,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_EQL,  KC_BSPC, KC_INSERT, KC_HOME, KC_PGUP,  KC_TRNS,  KC_TAB,   KC_Q,    KC_W,    KC_E,
                 KC_R,    KC_T,    KC_Y,      KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_DEL,
                 KC_END,  KC_PGDN, KC_TRNS,   KC_CAPS, KC_A,     KC_S,     KC_D,     KC_F,    KC_G,    KC_H,
-                KC_J,    KC_K,    KC_L,      KC_SCLN, KC_QUOT,  KC_NUHS,  KC_ENT,   KC_TRNS, KC_TRNS, KC_TRNS,
-                KC_TRNS, KC_LSFT, KC_Z,      KC_X,    KC_C,     KC_V,     KC_B,     KC_N,    KC_M,    KC_COMM,
-                KC_DOT,  KC_SLSH, KC_BSLS,   KC_RSFT, KC_TRNS,  KC_UP,    KC_TRNS,  KC_TRNS, KC_LCTL, KC_LGUI,
+                KC_J,    KC_K,    KC_L,      KC_SCLN, KC_QUOT,  KC_ENT,   KC_NUHS,  KC_TRNS, KC_TRNS, KC_TRNS,
+                KC_TRNS, KC_LSFT, KC_LSFT,   KC_Z,    KC_X,     KC_C,     KC_V,     KC_B,    KC_N,    KC_M,
+                KC_COMM, KC_DOT,  KC_SLSH,   KC_RSFT, KC_TRNS,  KC_UP,    KC_TRNS,  KC_TRNS, KC_LCTL, KC_LGUI,
                 KC_LALT, KC_SPC,  KC_RALT,   MO(_FN), KC_MENU,  KC_RCTL,  KC_LEFT,  KC_DOWN, KC_RIGHT
                 ),
 
@@ -78,6 +81,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                 _______, _______, _______, _______, _______, _______, RGB_SPD, RGB_VAD, RGB_SPI
             ),
+
+    [_ME]   = LAYOUT_411_iso(
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______
+            ),
+    [_SP]   = LAYOUT_411_iso(
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______, _______, _______, _______
+            ),
+
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
@@ -87,13 +116,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
         case P1:
             if(record->event.pressed)
             {
-                SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_R) SS_UP(X_LGUI) SS_DELAY(50) "1306" SS_TAP(X_ENTER));
+                //SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_R) SS_UP(X_LGUI) SS_DELAY(50) "1306" SS_TAP(X_ENTER));
+                SEND_STRING("1306" SS_TAP(X_ENTER));
             }
             break;
         case P2:
             if(record->event.pressed)
             {
-                SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_R) SS_UP(X_LGUI) SS_DELAY(50) "130656" SS_TAP(X_ENTER));
+                //SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_R) SS_UP(X_LGUI) SS_DELAY(50) "130656" SS_TAP(X_ENTER));
+                SEND_STRING("130656" SS_TAP(X_ENTER));
             }
             break;
         case P3:
@@ -102,6 +133,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
                 SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_R) SS_UP(X_LGUI) SS_DELAY(50) "open_sesame" SS_TAP(X_ENTER));
             }
             break;
+        default:
+            break;
     }
     return true;
 };
+
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
+};
+#endif
